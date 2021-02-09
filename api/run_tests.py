e5e1ba1ed285d7707eed1239fbc3b01f59f5d412
Konstantin <glaih2k@gmail.com>
2021-02-09 16:52:55 +0300

    Reworked pathing in create_db and some small fixes.
    +Create_db is a function now, that works from input or import.
    +Added space in test_app.py
    +Changed verbosity in run_tests.py to 2.

--- api/run_tests.py
+++ api/run_tests.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    result = unittest.TextTestRunner(verbosity=3).run(tests)
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
 
     if result.wasSuccessful():
         exit(0)
--- api/tests/create_db.py
+++ api/tests/create_db.py
@@ -1,11 +1,36 @@
 import sqlite3
 from func.registration import DB_PATH
+from pathlib import Path
+import logging
 
-conn = sqlite3.connect('../' + DB_PATH)
 
-c = conn.cursor()
+logger = logging.getLogger(__name__)
 
-c.execute('''CREATE TABLE auth (_id integer primary key autoincrement, mail text not null unique, psw text not null)''')
 
-conn.commit()
-conn.close()
+def db_name(input_name):
+    if '/' in list(input_name) or '\\' in list(input_name):
+        input_name = ''.join([e for e in list(input_name) if e != '/' and e != '\\'])
+
+    if input_name[-3:] == '.db':
+        return input_name
+    else:
+        return input_name + '.db'
+
+
+def create_base(name):
+    conn = sqlite3.connect(Path(DB_PATH).parent / db_name(name))
+    c = conn.cursor()
+
+    try:
+        c.execute('''CREATE TABLE auth (_id integer primary key autoincrement, mail text not null unique, psw text not null)''')
+    except sqlite3.OperationalError:
+        logger.exception("ERROR: 'Base already exist'")
+        exit(1)
+
+    conn.commit()
+    conn.close()
+    exit(0)
+
+
+if __name__ == '__main__':
+    create_base(str(input('Enter base name: ')))
