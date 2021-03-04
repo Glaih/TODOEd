19e412853066a042dc0562a1ff69aefd08f43352
Naryushkin Konstantin <glaih2k@gmail.com>
2021-03-04 11:55:10 +0300

    Now value of `SECRET_KEY` and `JWT_SECRET_KEY` assigns through keys

--- api/config.py
+++ api/config.py
@@ -9,8 +9,8 @@ TEST_DB_PATH = Path(__file__, '../db/test_users.db').resolve()
 class BaseConfig:
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_PATH}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = environ.get('SECRET')
-    JWT_SECRET_KEY = environ.get('JWT_SECRET')
+    SECRET_KEY = environ['SECRET']
+    JWT_SECRET_KEY = environ['JWT_SECRET']
     JWT_TOKEN_LOCATION = ["json"]
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=2)
